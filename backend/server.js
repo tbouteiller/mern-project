f9ef3680442bb7bd6a9e71cf29dfba3e59df58e7
Tanner <tannerbouteiller@gmail.com>
2022-07-06 10:52:59 -0500

    added MongoDB connection and installed mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes/workouts");
 
 //create an express app
 const app = express();
 
 //MIDDLEWARE
-//checks if request has data and attaches to req object
-app.use(express.json());
-//fires for every request and logs path and method
+app.use(express.json()); //checks if request has data and attaches to req object
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-});
+}); //fires for every request and logs path and method
 
 //fire request to /api/workouts & then uses router
 app.use("/api/workouts", routes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Running on Port", process.env.PORT);
-});
+//connect to db -> returns a promise -> only listen for requests when db connects
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB. Listening on port:", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
