715c2487e9fbf22b861083e450df1afc82ae9d24
Tanner <tannerbouteiller@gmail.com>
2022-07-28 13:39:39 -0500

    created the service helper for authentication

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "/api/users/";
+
+//@type REGISTER service function
+//@route /api/users
+//@desc sends POST request to server and stores data, including the token, in response.data
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//@type LOGIN service function
+//@route /api/users/login
+//@desc sends POST request to server and stores data, including the token, in response.data
+const login = async (userData) => {
+  const response = await axios.post(`${API_URL}login`, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//@type LOGIN service function
+//@desc Removes user from local storage
+const logout = async () => {
+  localStorage.removeItem("user");
+};
+
+const authService = { register, login, logout };
+export default authService;
